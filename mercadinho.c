3cf5ffec059500e43bef2600470ac4b3e064d9ab
Lucas <lucasrdsprof@gmail.com>
2026-01-22 15:55:17 -0300

    Atualização no projeto

--- mercadinho.c
+++ mercadinho.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char traco(int quantidade){
+char traco(int quantidade){ //Creation of the "dash" function, to make the code more organized
     for(int i = 0; i < quantidade; i++){
         printf("-");
     }
@@ -9,7 +9,7 @@ char traco(int quantidade){
 }
 
 int main(){
-    struct produtos{
+    struct produtos{ //Creation of a Struct for the products
         char nome[50];
         char codigo [5];
         float preco;
@@ -18,14 +18,14 @@ int main(){
     int op, i, totalprod = 0;
     struct produtos prod[100];
 
-    while(op != 4){
+while(op != 4){
     traco(60);
-    printf("[1] Cadastrar produto \n[2] Comprar produto \n[3] Listar produtos \n[4] Sair \n-> Digite a opção certa: ");
+    printf("[1] Cadastrar produto \n[2] Comprar produto \n[3] Listar produtos \n[4] Sair \n-> Digite a opção certa: "); //Main
     scanf("%d", &op);
     traco(60);
 
     switch (op) {
-    case 1: {
+    case 1: { //register product
         int qntdprod;
         printf("Digite a quantidade de produtos que você deseja cadastrar: ");
         scanf("%d", &qntdprod);
@@ -45,44 +45,42 @@ int main(){
         break;
     }
 
-    case 2: {
+    case 2: { //buy product
         char verificacaocod[5];
         int qntdcomprada = 0;
         printf("Digite o código do produto que você quer comprar: ");
         scanf("%s", verificacaocod);
 
-        for(i = 0; i < totalprod; i++){
+        for(i = 0; i < totalprod; i++){ 
 
-            if (strcmp(verificacaocod, prod[i].codigo) == 0){ 
+            if (strcmp(verificacaocod, prod[i].codigo) == 0){ //Product code validation
                 printf("Digite a quantidade para ser comprada: ");
                 scanf("%d", &qntdcomprada);
 
-                if(qntdcomprada <= prod[i].estoque){
+                if(qntdcomprada <= prod[i].estoque){ //Checking if there is stock for purchase
                     printf("Compra de %s registrada!\n", prod[i].nome);
                     prod[i].estoque -= qntdcomprada;
                 }
 
-                else{
+                else{ //Insufficient stock
                     printf("Quantidade insuficiente, temos apenas %d no estoque!\n", prod[i].estoque);
                 }
         }
+            else{ //Nonexistent code
+                printf("O código do produto não existe!\n");
+            }
     }
-
-        if(qntdcomprada = 0){
-            printf("O Código não existe!\n");
-        }
         break;
     }
 
-    case 3:{
+    case 3:{ //list product
         for(i = 0; i < totalprod; i++){
             printf("%dº produto: %s\nEstoque: %d\n", (i+1), prod[i].nome, prod[i].estoque);
-            traco(60);
         }
         break;
     }
     
-    case 4:{
+    case 4:{ //close
         printf("Fechando o sistema...");
         break;
     }
